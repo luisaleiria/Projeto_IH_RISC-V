3c6eadcdc4a1d7bde0be341a6ca9adf01b1b35d1
Luisa Leiria <112179923+luisaleiria@users.noreply.github.com>
2023-08-09 13:38:54 -0300

    Update ALUController.sv

--- design/ALUController.sv
+++ design/ALUController.sv
@@ -1,3 +1,4 @@
+
 `timescale 1ns / 1ps
 
 module ALUController (
@@ -10,19 +11,21 @@ module ALUController (
     output logic [3:0] Operation  // operation selection for ALU
 );
 
-  assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110)) ||  // R\I-or
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000)) ||  // R\I->>
+  assign Operation[0] = ((ALUOp == 2'b10) && (Funct3 == 3'b110))   // R\I-or 
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0000000))   // R\I->>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000) && (Funct7 == 7'b0100000)); // R\I-sub
+
+  assign Operation[1] = (ALUOp == 2'b00)   // LW\SW
+      ((ALUOp == 2'b10) && (Funct3 == 3'b000))   // R\I-add + sub
       ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
 
-  assign Operation[1] = (ALUOp == 2'b00) ||  // LW\SW
-      ((ALUOp == 2'b10) && (Funct3 == 3'b000)) ||  // R\I-add
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000));  // R\I->>>
+  assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000))  // R\I->>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000))   // R\I->>>
+      ((ALUOp == 2'b10) && (Funct3 == 3'b001))   // R\I-<<
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010))  // R\I-< + slt
+      ((ALUOp == 2'b10) && (Funct3 == 3'b100)); // R\I- xor
 
-  assign Operation[2] =  ((ALUOp==2'b10) && (Funct3==3'b101) && (Funct7==7'b0000000)) || // R\I->>
-      ((ALUOp == 2'b10) && (Funct3 == 3'b101) && (Funct7 == 7'b0100000)) ||  // R\I->>>
-      ((ALUOp == 2'b10) && (Funct3 == 3'b001)) ||  // R\I-<<
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
-
-  assign Operation[3] = (ALUOp == 2'b01) ||  // BEQ
-      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-<
+  assign Operation[3] = (ALUOp == 2'b01)   // BEQ
+      ((ALUOp == 2'b10) && (Funct3 == 3'b010));  // R\I-< + slt
 endmodule
