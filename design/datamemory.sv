6ae0ab4adb661595bfbc23877914116ae2596d85
Luisa Leiria <112179923+luisaleiria@users.noreply.github.com>
2023-08-29 18:35:56 -0300

    Update datamemory.sv

--- design/datamemory.sv
+++ design/datamemory.sv
@@ -11,6 +11,7 @@ module datamemory #(
     input logic [DATA_W - 1:0] wd,  // Write Data
     input logic [2:0] Funct3,  // bits 12 to 14 of the instruction
     output logic [DATA_W - 1:0] rd  // Read Data
+    //input logic [15:0] tmp //temporario para sh
 );
 
   logic [31:0] raddress;
@@ -28,6 +29,12 @@ module datamemory #(
       .Wr(Wr)
   );
 
+  assign in3 = Datain[31:24];
+  assign in2 = Datain[23:16];
+  assign in1 = Datain[15:8];
+  assign in0 = Datain[7:0];
+
+
   always_ff @(*) begin
     raddress = {{22{1'b0}}, a};
     waddress = {{22{1'b0}}, {a[8:2], {2{1'b0}}}};
@@ -37,14 +44,39 @@ module datamemory #(
     if (MemRead) begin
       case (Funct3)
         3'b010:  //LW
-        rd <= Dataout;
+          rd <= Dataout;
+	      3'b000: //LB
+          rd <= $signed(Dataout[7:0]);
+	        //if(Dataout[7]==0) rd <= {{24{1'b0}}, Dataout[7:0]};
+          //else rd <=  {{24{1'b1}}, Dataout[7:0]};
+        3'b001:  //LH
+          rd <= $signed(Dataout[15:0]);
+          //if(Dataout[15]==0) rd <= {{16{1'b0}}, Dataout[15:0]};
+          //else rd <=  {{16{1'b1}}, Dataout[15:0]};
+        3'b100:   //LBU
+          rd <= $unsigned(Dataout[7:0]);
+          //rd <= {{24{1'b0}}, Dataout[7:0]}; 
+        3'b101:    //LHU
+          rd <= $unsigned(Dataout[15:0]);
+          //rd <= {{16{1'b0}}, Dataout[15:0]};
         default: rd <= Dataout;
       endcase
+    
     end else if (MemWrite) begin
       case (Funct3)
         3'b010: begin  //SW
           Wr <= 4'b1111;
           Datain <= wd;
+        end
+	      3'b001: begin //SH
+	        Wr <= 4'b1100;
+          Datain[15:0] <= wd;
+          
+        end
+        3'b000: begin //SB
+	        Wr <= 4'b0100;
+          Datain[7:0] <= wd;
+          //Datain <= {wd[7] ? 24'hFFFFFF : 24'b0, wd[7:0]};
         end
         default: begin
           Wr <= 4'b1111;
@@ -55,3 +87,4 @@ module datamemory #(
   end
 
 endmodule
+
