96af96f0b9dd490d4dabc767c6ef847de4bf5487
Luisa Leiria <112179923+luisaleiria@users.noreply.github.com>
2023-08-29 21:54:04 -0300

    Update imm_Gen.sv

--- design/imm_Gen.sv
+++ design/imm_Gen.sv
@@ -1,10 +1,10 @@
+
 `timescale 1ns / 1ps
 
 module imm_Gen (
     input  logic [31:0] inst_code,
     output logic [31:0] Imm_out
 );
-
 
   always_comb
     case (inst_code[6:0])
@@ -23,6 +23,28 @@ module imm_Gen (
         inst_code[11:8],
         1'b0
       };
+
+      7'b0010011: //I_TYPE
+      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+
+      7'b1101111: //J-type
+      Imm_out = {
+        inst_code[31] ? 11'h7FF : 11'b0,
+        inst_code[31],
+        inst_code[19:12],
+        inst_code[20],
+        inst_code[30:21],
+        1'b0
+      };
+
+      7'b1100111: //JALR
+      Imm_out = {inst_code[31] ? 20'hFFFFF : 20'b0, inst_code[31:20]};
+
+
+
+      7'b0110111: // U_TYPE
+      Imm_out = {inst_code[31:12],12'b0};
+
 
       default: Imm_out = {32'b0};
 
